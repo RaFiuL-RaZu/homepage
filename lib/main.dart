fb4a3cf0ffed1407d63093e22f9eb224201fca4a
RaFiuL-RaZu <rafi102621@gmail.com>
2025-02-10 23:42:51 +0600

    innit commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,27 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-main(){
+main() {
   runApp(const MyApp());
 }
-class MyApp extends StatelessWidget{
+
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Homescren(),
     );
   }
-
 }
 
-class Homescren extends StatelessWidget{
+class Homescren extends StatelessWidget {
   const Homescren({Key? key}) : super(key: key);
-
-
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.orangeAccent.shade100,
@@ -46,7 +41,9 @@ class Homescren extends StatelessWidget{
                 height: 100,
                 width: double.infinity,
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(bottomRight: Radius.circular(20.0),bottomLeft: Radius.circular(20.0)),
+                  borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(20.0),
+                      bottomLeft: Radius.circular(20.0)),
                   color: Colors.orangeAccent.shade100,
                 ),
                 child: Padding(
@@ -59,14 +56,25 @@ class Homescren extends StatelessWidget{
                           Center(
                             child: CircleAvatar(
                               radius: 40,
-                              backgroundImage: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeQ-HkOk0nyWwdR6GNhI19KyuIDOyg-_w_tQ&s"),
+                              backgroundImage: NetworkImage(
+                                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeQ-HkOk0nyWwdR6GNhI19KyuIDOyg-_w_tQ&s"),
                             ),
                           ),
-                          SizedBox(width:5,),
+                          SizedBox(
+                            width: 5,
+                          ),
                           Column(
                             children: [
-                              Text("RaFiuL Isalam Razu",style: TextStyle(fontWeight: FontWeight.w800,fontSize: 25),),
-                              Text("App Developer",style: TextStyle(fontSize: 18,fontWeight: FontWeight.w500),),
+                              Text(
+                                "RaFiuL Isalam Razu",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w800, fontSize: 25),
+                              ),
+                              Text(
+                                "App Developer",
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.w500),
+                              ),
                             ],
                           ),
                         ],
@@ -80,11 +88,18 @@ class Homescren extends StatelessWidget{
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text("My Tasks",style: TextStyle(fontWeight: FontWeight.w800,fontSize: 23),),
+                    Text(
+                      "My Tasks",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w800, fontSize: 23),
+                    ),
                     Container(
                       height: 40,
                       width: 40,
-                      child: Icon(Icons.account_box,color: Colors.white,),
+                      child: Icon(
+                        Icons.account_box,
+                        color: Colors.white,
+                      ),
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(50),
                         color: Colors.teal,
@@ -102,70 +117,106 @@ class Homescren extends StatelessWidget{
                         Container(
                           height: 50,
                           width: 50,
-                          child: Icon(Icons.alarm,color: Colors.white,),
+                          child: Icon(
+                            Icons.alarm,
+                            color: Colors.white,
+                          ),
                           decoration: BoxDecoration(
                             color: Colors.redAccent,
                             borderRadius: BorderRadius.circular(50),
                           ),
                         ),
-                        SizedBox(width: 15,),
+                        SizedBox(
+                          width: 15,
+                        ),
                         Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text("To Do",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w800),),
-                            SizedBox(height: 5,),
+                            Text(
+                              "To Do",
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w800),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
                             Text("5 task now,1 started"),
                           ],
                         ),
                       ],
                     ),
-                    SizedBox(height: 20,),
+                    SizedBox(
+                      height: 20,
+                    ),
                     Row(
                       children: [
                         Container(
                           height: 50,
                           width: 50,
-                          child: Icon(Icons.alarm,color: Colors.white,),
+                          child: Icon(
+                            Icons.alarm,
+                            color: Colors.white,
+                          ),
                           decoration: BoxDecoration(
                             color: Colors.blueAccent,
                             borderRadius: BorderRadius.circular(50),
                           ),
                         ),
-                        SizedBox(width: 15,),
+                        SizedBox(
+                          width: 15,
+                        ),
                         Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text("In Progress",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w800),),
-                            SizedBox(height: 5,),
+                            Text(
+                              "In Progress",
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w800),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
                             Text("1 task now,now started"),
                           ],
                         ),
                       ],
                     ),
-                    SizedBox(height: 20,),
+                    SizedBox(
+                      height: 20,
+                    ),
                     Row(
                       children: [
                         Container(
                           height: 50,
                           width: 50,
-                          child: Icon(Icons.alarm,color: Colors.white,),
+                          child: Icon(
+                            Icons.alarm,
+                            color: Colors.white,
+                          ),
                           decoration: BoxDecoration(
                             color: Colors.amber,
                             borderRadius: BorderRadius.circular(50),
                           ),
                         ),
-                        SizedBox(width: 15,),
+                        SizedBox(
+                          width: 15,
+                        ),
                         Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text("Done",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w800),),
-                            SizedBox(height: 5,),
+                            Text(
+                              "Done",
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w800),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
                             Text("18 task now,13 started"),
                           ],
                         ),
                       ],
                     ),
-
                   ],
                 ),
               ),
@@ -173,8 +224,14 @@ class Homescren extends StatelessWidget{
                 padding: const EdgeInsets.all(8.0),
                 child: Row(
                   children: [
-                    SizedBox(width: 5,),
-                    Text("Active Projects",style: TextStyle(fontWeight: FontWeight.w800,fontSize: 20),),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      "Active Projects",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w800, fontSize: 20),
+                    ),
                   ],
                 ),
               ),
@@ -185,7 +242,7 @@ class Homescren extends StatelessWidget{
                     GridView(
                       physics: NeverScrollableScrollPhysics(),
                       shrinkWrap: true,
-                      gridDelegate:SliverGridDelegateWithFixedCrossAxisCount(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                           crossAxisCount: 2),
                       children: [
                         Padding(
@@ -197,27 +254,35 @@ class Homescren extends StatelessWidget{
                               borderRadius: BorderRadius.circular(20),
                               color: Colors.teal,
                             ),
+                            child: Center(child: Text("Medical App",style: TextStyle(color: Colors.white,fontSize: 18),)),
                           ),
                         ),
-                        Container(
-                          height: 100,
-                          width: 50,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            color: Colors.teal,
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            height: 100,
+                            width: 50,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.orange,
+                            ),
+                            child: Center(child: Text("Making History Notes",style: TextStyle(color: Colors.white,fontSize: 18),)),
                           ),
                         ),
-                        Container(
-                          height: 100,
-                          width: 50,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            color: Colors.teal,
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            height: 100,
+                            width: 50,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.teal,
+                            ),
+                            child: Center(child: Text("Medical App",style: TextStyle(color: Colors.white,fontSize: 18),)),
                           ),
                         ),
                       ],
                     ),
-
                   ],
                 ),
               ),
@@ -225,8 +290,6 @@ class Homescren extends StatelessWidget{
           ),
         ),
       ),
-
     );
   }
-
 }
