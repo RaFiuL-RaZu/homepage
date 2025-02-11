99d820fb821b28a1c8d15987d4fcf60ccdb91ab2
RaFiuL-RaZu <rafi102621@gmail.com>
2025-02-11 10:17:31 +0600

    innit commit

--- lib/main.dart
+++ lib/main.dart
@@ -33,236 +33,26 @@ class Homescren extends StatelessWidget {
         ],
       ),
       drawer: Drawer(),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                height: 100,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      bottomRight: Radius.circular(20.0),
-                      bottomLeft: Radius.circular(20.0)),
-                  color: Colors.orangeAccent.shade100,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ListView(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Center(
-                            child: CircleAvatar(
-                              radius: 40,
-                              backgroundImage: NetworkImage(
-                                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeQ-HkOk0nyWwdR6GNhI19KyuIDOyg-_w_tQ&s"),
-                            ),
-                          ),
-                          SizedBox(
-                            width: 5,
-                          ),
-                          Column(
-                            children: [
-                              Text(
-                                "RaFiuL Isalam Razu",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w800, fontSize: 25),
-                              ),
-                              Text(
-                                "App Developer",
-                                style: TextStyle(
-                                    fontSize: 18, fontWeight: FontWeight.w500),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "My Tasks",
-                      style:
-                          TextStyle(fontWeight: FontWeight.w800, fontSize: 23),
-                    ),
-                    Container(
-                      height: 40,
-                      width: 40,
-                      child: Icon(
-                        Icons.account_box,
-                        color: Colors.white,
-                      ),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(50),
-                        color: Colors.teal,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Container(
-                          height: 50,
-                          width: 50,
-                          child: Icon(
-                            Icons.alarm,
-                            color: Colors.white,
-                          ),
-                          decoration: BoxDecoration(
-                            color: Colors.redAccent,
-                            borderRadius: BorderRadius.circular(50),
-                          ),
-                        ),
-                        SizedBox(
-                          width: 15,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "To Do",
-                              style: TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.w800),
-                            ),
-                            SizedBox(
-                              height: 5,
-                            ),
-                            Text("5 task now,1 started"),
-                          ],
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      children: [
-                        Container(
-                          height: 50,
-                          width: 50,
-                          child: Icon(
-                            Icons.alarm,
-                            color: Colors.white,
-                          ),
-                          decoration: BoxDecoration(
-                            color: Colors.blueAccent,
-                            borderRadius: BorderRadius.circular(50),
-                          ),
-                        ),
-                        SizedBox(
-                          width: 15,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "In Progress",
-                              style: TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.w800),
-                            ),
-                            SizedBox(
-                              height: 5,
-                            ),
-                            Text("1 task now,now started"),
-                          ],
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      children: [
-                        Container(
-                          height: 50,
-                          width: 50,
-                          child: Icon(
-                            Icons.alarm,
-                            color: Colors.white,
-                          ),
-                          decoration: BoxDecoration(
-                            color: Colors.amber,
-                            borderRadius: BorderRadius.circular(50),
-                          ),
-                        ),
-                        SizedBox(
-                          width: 15,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Done",
-                              style: TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.w800),
-                            ),
-                            SizedBox(
-                              height: 5,
-                            ),
-                            Text("18 task now,13 started"),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    SizedBox(
-                      width: 5,
-                    ),
-                    Text(
-                      "Active Projects",
-                      style:
-                          TextStyle(fontWeight: FontWeight.w800, fontSize: 20),
-                    ),
-                  ],
-                ),
-              ),
-              Column(
+      body:GridView(
+        gridDelegate:SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        children: [
+          Card(
+            color: Colors.grey,
+            child: SizedBox(
+              height: 100,
+              width: 100,
+              child: Column(
                 children: [
-                  GridView(
-                      gridDelegate:SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-                    children: [
-                      Card(
-                        color: Colors.grey,
-                        child: SizedBox(
-                          height: 100,
-                          width: 100,
-                          child: Column(
-                            children: [
-                              Image.network("https://t4.ftcdn.net/jpg/02/81/42/77/360_F_281427785_gfahY8bX4VYCGo6jlfO8St38wS9cJQop.jpg"),
-                              Text("Medical App"),
-                              Text("5 Task Now"),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-
+                  Image.network("https://t4.ftcdn.net/jpg/02/81/42/77/360_F_281427785_gfahY8bX4VYCGo6jlfO8St38wS9cJQop.jpg",
+                  height: 100,
+                  width: 50,),
+                  Text("Medical App"),
+                  Text("5 Task Now"),
                 ],
               ),
-
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
